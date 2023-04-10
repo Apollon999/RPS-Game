934edc84ac08d165c683f9d309eae9361adf6945
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-04-10 18:16:03 +0000

    add the fuction that makes the computer randomly choose an option

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
 
 
 function getComputerChoice () {
-
+   const rpsChoice =['Rock', 'Paper', 'Scissors'] 
+   const randomNumber = Math.floor(Math.random() * 3)
+   return rpsChoice[randomNumber]
 }
 
 function getResult () {
+    
+    let winner;
+    if (playerChoice == computerChoice) {
+        winner = tie;
+    } else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
+        winner = 'player';
+    } else if (playerChoice == 'Paper' && computerChoice == 'Rock') {
+        winner = 'player';
+    } else if (playerChoice == 'Scissors' && computerChoice == 'Paper') {
+        winner = 'player';
+    } else {
+        winner = 'computer';
+    }
+    return winner;
+}
 
+let result = getResult(playerChoice, computerChoice);
+if (result == 'player') {
+    playerScore++;
+    document.getElementById('player-score').textContent = playerScore;
+} else if (result == 'computer') {
+    computerScore++;
+    document.getElementById('computer-score').textContent = computerScore;
 }
 
 function showResult () {
