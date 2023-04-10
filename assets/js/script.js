302ab1ae1e5f5f39dc6a4a6ffc9679bb1ebca1df
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-04-10 16:30:15 +0000

    add functions to js

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+
+function getComputerChoice () {
+
+}
+
+function getResult () {
+
+}
+
+function showResult () {
+
+}
+
+function onClickRPS(PlayerChoice) {
+
+}
+
+function playGame () {
+
+}
+
+
+
+
